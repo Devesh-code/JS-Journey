7560427998e2a40a4878c88128c6f069367d6119
Devesh-code <developersharma1999@gmail.com>
2024-09-17 04:09:48 +0530

    done with dom and 2 basic projects to understand dom manupulation

---
+++ domProjects/colorChanger/app.js
@@ -0,0 +1,33 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+// buttons.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     if (e.target.id === "grey") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "red") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "blue") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "purple") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "green") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//     if (e.target.id === "yellow") {
+//       body.style.backgroundColor = e.target.id;
+//     }
+//   });
+// });
+
+// improved code
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    body.style.backgroundColor = e.target.id;
+  });
+});
