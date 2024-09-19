4098cbd1f26bb37676cf91e96a248777e89522f5
Devesh-code <developersharma1999@gmail.com>
2024-09-20 01:00:01 +0530

    going deep in async javaScript

--- domProjects/colorChanger/app.js
+++ domProjects/colorChanger/app.js
@@ -27,7 +27,24 @@ const body = document.querySelector("body");
 // improved code
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    body.style.backgroundColor = e.target.id;
-  });
+  button.addEventListener(
+    "click",
+    (e) => {
+      // this a best approach to add events to single or multiple elements using addEventListener which is more efficient than using onclick.
+      body.style.backgroundColor = e.target.id;
+    },
+    false,
+  );
 });
+
+// events : type of events
+// click, mouseover, mouseout, keydown, keyup, keypress, focus, blur, submit, change, load, resize, scroll, etc.
+// type, target, currentTarget, preventDefault, stopPropagation, defaultPrevented, etc.
+// clineX, clientY, pageX, pageY, screenX, screenY, etc.
+// key, code, keyCode, charCode, etc.
+//
+// event propagation:
+// bubbling: when an event is triggered on an element, it will bubble up to the parent elements.
+// capturing: when an event is triggered on an element, it will be captured by the parent elements.
+// stopPropagation: stops the event from bubbling up or capturing.
+// preventDefault: prevents the default behavior of the event.
